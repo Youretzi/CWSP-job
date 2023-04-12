02343bf2c0f42df30bb2c6e3e1c0977b43d22bc4
Youretzi <120606556+Youretzi@users.noreply.github.com>
2023-04-12 10:23:08 -0700

    Easter (#7)
    * Pass
    
    * Updated version
    
    * Updated version 2
    
    ---------
    
    Co-authored-by: CRRE - CWSPStudentWorkerMBA1 <cwspstudentworkermba1@CRRE-CWSP-WorkerMBA1.local>
    Co-authored-by: CRRE - CWSPStudentWorkerMBA1 <cwspstudentworkermba1@CRRECWSrkerMBA1.hsd1.ca.comcast.net>

--- Hangman.py
+++ Hangman.py
@@ -1,77 +1,125 @@
 import random
 
 # Constants here 
-VALID_WORDS = ['test', 'other', "halloween", "series", "accomplice", "show"]
+
+VALID_WORDS = ['tests', 'others', "halloweens", "series", "accomplices", "shows"]
 MAX_NUMBER_OF_GUESSES = 6
 
 # ex: keep track of user's inputs, potential list of words to choose from, etc...
-USERS_INPUTS = []
-r = random.choice(VALID_WORDS)
+USERS_INPUTS = [""]
+
 # Pick a word for hangman round
-def pick_word(r):
-    #return a word from VALID_WORDS
-   
-    print(r)
-    return r
-    pass
+def pick_word():
+    r = random.choice(VALID_WORDS)
+
 
 # Create a function to draw the guillotine
-def draw_guillotine():
-    print(  '   _____ \n'
-            '  |      \n'
-            '  |      \n'
+
+def draw_guillotine(counter):
+    if counter == 0:
+        print(  '   _____ \n'
+                '  |      \n'
+                '  |      \n'
+                '  |      \n'
+                '  |      \n'
+                '  |      \n'
+                '  |      \n'
+                '__|__\n')   
+    elif counter == 1:  
+        print('   _____ \n'
+            '  |     | \n'
+            '  |     | \n'
             '  |      \n'
             '  |      \n'
             '  |      \n'
             '  |      \n'
             '__|__\n')
-    pass
-
+    elif counter == 2:
+        print('   _____ \n'
+            '  |     | \n'
+            '  |     | \n'
+            '  |     | \n'
+            '  |      \n'
+            '  |      \n'
+            '  |      \n'
+            '__|__\n')
+    elif counter == 3:
+        print('   _____ \n'
+            '  |     | \n'
+            '  |     | \n'
+            '  |     | \n'
+            '  |     O \n'
+            '  |      \n'
+            '  |      \n'
+            '__|__\n')
+    elif counter == 4:
+        print('   _____ \n'
+                '  |     | \n'
+                '  |     | \n'
+                '  |     | \n'
+                '  |     O \n'
+                '  |    /|\ \n'
+                '  |    / \ \n'
+                '__|__\n')
+        
 
 # Create a function to take the user's inputs (letters)
 def accept_letter(r):
-    #make sure to reject if user inputs a number or more than one character
-    a = input("Guess: ")
-    ch = a
-    while a.isnumeric() is True:
-        a = input("Error, Enter New input: ")
-        print(a)
-    if len(a) != 1:
-        a = input("Error, Enter New input: ")
-        print(a)
-    elif ch in r: 
-        d = input("Correct! Try new input: ")
-        print(d)
-    else:
-        print('   _____ \n'
-              '  |     | \n'
-              '  |     | \n'
-              '  |      \n'
-              '  |      \n'
-              '  |      \n'
-              '  |      \n'
-              '__|__\n')
-    pass
+    while True:
+        a = input("Guess: ")
+        ch = a
+        if a.isnumeric() is True:
+             print("Error, No numbers in input")
+        elif len(a) != 1:
+            print("Error input must be one character")   
+        elif a.lower() in USERS_INPUTS:
+            print("This letter has been gussed")
+        elif ch in r:
+            USERS_INPUTS.append(a.lower())
+            print("Correct, guess again: ")
+            return True
+        else:
+            print("letter not in word")
+            USERS_INPUTS.append(ch)
+            return False 
 
 
-# Create a function to play out each round
-def play_round():
-    pass
+#This draws the underscores and replaces them once a letter is correctly guessed. Ex: t _ _ s _ _
+def print_answer(r):
+    c = ""
+    for x in r:
+        if x in USERS_INPUTS:
+            c += x 
+        else: 
+            c += "_"
+    print (c)
+
+
+# Checks if the answer/input is correct 
+def check_answer(r):
+    missing_word = True
+    for x in r:
+        if x not in USERS_INPUTS:
+            missing_word = False
+    return missing_word
 
 
 # What actually runs when we use "python3 Hangman.py"
-if __name__ == '__main__':
+if __name__ == '__main__': 
+    r = pick_word()
+    counter = 0 
     print("Starting hangman script!")
-    print(pick_word(r))
-    print(draw_guillotine())
-    print(accept_letter(r))
-    print(play_round())
+    while check_answer(r) is False:
+        if accept_letter(r) == False:
+            counter += 1 
+        draw_guillotine(counter)
+        if counter == 4:
+            print("You Lost")
+            break 
+        print_answer(r)
+        #print(play_round())
+        check_answer(r)
+        print(counter)
+    if check_answer(r) is True:
+        print("Congrats! You won!")
 
-    # pick word
-    # draw guillotine with underscores
-    # for loop for max guesses
-        # take user input
-        # redraw hangman
-        # check if guessed word is same as picked word
-            # if guessed correctly, print you win and exit for loop using 'break'
-    # if guessed word not picked word then print game over and print dead hangman
